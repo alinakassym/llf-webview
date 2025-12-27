2e250d0e6c320a0cc12b9927a2f16b6ba649767f
alinakassym <alinakassym@gmail.com>
2025-12-28 02:01:10 +0500

    refactor(auth): move Firebase token auth to global scope in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
+import { StrictMode, type FC, type ReactNode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import ThemeWrapper from './components/ThemeWrapper'
 import { ReauthProvider } from './contexts/ReauthContext'
+import { useFirebaseTokenAuth } from './hooks/useFirebaseTokenAuth'
 import App from './App'
+
+// Обёрточный компонент для глобальных хуков
+const RootWrapper: FC<{ children: ReactNode }> = ({ children }) => {
+  useFirebaseTokenAuth();
+  return <>{children}</>;
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,7 +20,9 @@ createRoot(document.getElementById('root')!).render(
       <ThemeWrapper>
         <BrowserRouter>
           <ReauthProvider>
-            <App />
+            <RootWrapper>
+              <App />
+            </RootWrapper>
           </ReauthProvider>
         </BrowserRouter>
       </ThemeWrapper>
